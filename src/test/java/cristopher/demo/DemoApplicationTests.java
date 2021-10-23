9d8c007e61a72bf0d11c3b2688e70825f926a54f
CristopherGS <guerracristofer@gmail.com>
2021-10-23 13:22:55 -0600

    probando IDE y springboot

---
+++ src/test/java/cristopher/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cristopher.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
